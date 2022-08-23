a2fb2eb1c39328ffcc61d220b32bf80ca09d861b
V-Labs.ai-sensorAi <v-labsai-sensorai@V-Labs.ai-sensorAi>
2022-08-23 11:06:36 +0530

    comparing float16 and float32 operations time

--- float32.py
+++ float32.py
@@ -10,27 +10,57 @@ if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
+
+
+sizes = [
+    [4096, 4048, 4096],
+    [4096, 4056, 4096],
+    [4096, 4080, 4096],
+    [4096, 4096, 4096],
+    [4096, 4104, 4096],
+    [4096, 4128, 4096],
+    [4096, 4144, 4096],
+    [4096, 5096, 4096],
+    [4096, 5104, 4096],
+    [4096, 5112, 4096],
+    [4096, 5120, 4096],
+    [4096, 9728, 4096],
+    [4096, 16384, 4096],
+]
 # %%
 
-def calculate_time(arithmetic, n):
-    start = time.time()
 
-    A = torch.rand((10000, 10000),  dtype = arithmetic,device=device)
+def calculate_time(arithmetic, n, size):
 
-    x = torch.rand((10000, 5000),dtype = arithmetic, device=device)
+    A = torch.rand(size=(size[0], size[1]), dtype=arithmetic, device=device)
+
+    x = torch.rand(size=(size[1], size[2]), dtype=arithmetic, device=device)
 
     Ap = nn.Parameter(A, requires_grad=False)
-    xp = nn.Parameter(x,requires_grad=False)
+    xp = nn.Parameter(x, requires_grad=False)
 
+    prod = torch.mm(Ap, xp)
+
+    start = time.time()
 
     for _ in range(n):
-        sum = Ap @ xp 
-
+        prod = Ap @ xp
 
     end = time.time()
 
-    return end-start
-
+    return (end - start) / n
 
 
 # %%
+
+print(f"---------------------------------------------------------------")
+print(f"|         size           |    fp16_time     |    fp32_time    |")
+print(f"---------------------------------------------------------------")
+for size in sizes:
+    fp_16_time = calculate_time(torch.float16, 1000, size=size)
+    fp_32_time = calculate_time(torch.float32, 1000, size=size)
+    print(
+        f"| [{size[0]}],[{size[1]}],[{size[2]}]   |     {round(fp_16_time, 4)}       |      {round(fp_32_time,4)}     |"
+    )
+
+print(f"---------------------------------------------------------------")
--- test.py
+++ test.py
@@ -8,8 +8,10 @@ def calculate_time(arithmetic, n, tf32: bool):
 
     if tf32:
         torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
     else:
         torch.backends.cuda.matmul.allow_tf32 = False
+        torch.backends.cudnn.allow_tf32 = False
 
     start = time.time()
     for _ in range(n):
@@ -20,6 +22,20 @@ def calculate_time(arithmetic, n, tf32: bool):
     return end - start
 
 
+# with_tf_32 = calculate_time(torch.float32, 1000, True)
+# print(f"with_tf32_time: {with_tf_32/1000} ")
+
+
+without_tf_32 = calculate_time(torch.float32, 1000, False)
+print(f"without_tf_32_time: {without_tf_32/1000}")
+
+
+# import torch
+# import time
+
+# a_full = torch.randn(10240, 10240, dtype=torch.double, device="cuda")
+# b_full = torch.randn(10240, 10240, dtype=torch.double, device="cuda")
+# ab_full = a_full @ b_full
 # mean = ab_full.abs().mean()  # 80.7277
 
 # a = a_full.float()
@@ -27,20 +43,24 @@ def calculate_time(arithmetic, n, tf32: bool):
 
 # # Do matmul at TF32 mode.
 
+# torch.backends.cuda.matmul.allow_tf32 = True
+# start = time.time()
 # ab_tf32 = a @ b  # takes 0.016s on GA100
+# end = time.time()
+# print(end - start)
 # error = (ab_tf32 - ab_full).abs().max()  # 0.1747
 # relative_error = error / mean  # 0.0022
 
+# print(relative_error)
+
 # # Do matmul with TF32 disabled.
 # torch.backends.cuda.matmul.allow_tf32 = False
+# start = time.time()
+
 # ab_fp32 = a @ b  # takes 0.11s on GA100
+# end = time.time()
+# print(end - start)
 # error = (ab_fp32 - ab_full).abs().max()  # 0.0031
 # relative_error = error / mean  # 0.000039
 
-
-with_tf_32 = calculate_time(torch.float, 1000, True)
-print(f"with_tf32_time: {with_tf_32} ")
-
-
-# without_tf_32 = calculate_time(torch.float, 1000, False)
-# print(f"without_tf_32_time: {without_tf_32}")
+# print(relative_error)
